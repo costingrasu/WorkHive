038b15ffa945ee57650536536fd0ab9778a3b8a6
Grasu Costin-Alexandru <grasucostin@gmail.com>
2025-11-14 23:42:52 +0200

    added sql instead of orm for user

--- backend/src/main/java/com/workhive/api/repositories/UserRepository.java
+++ backend/src/main/java/com/workhive/api/repositories/UserRepository.java
@@ -2,8 +2,15 @@ package com.workhive.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.workhive.api.entities.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+    @Query(
+            value = "SELECT * FROM users WHERE email = :email",
+            nativeQuery = true
+    )
+    Optional<User> findByEmail(@Param("email") String email);
 }
